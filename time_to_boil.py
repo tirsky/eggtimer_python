83c4c25b458609563e2cba7c44ea2024bb4a61d6
Igor Tirsky <tirsky@gmail.com>
2022-01-30 03:15:46 +0300

    Update time_to_boil.py
    fix some code and pep8

--- time_to_boil.py
+++ time_to_boil.py
@@ -1,18 +1,18 @@
 import math
 
 
-def calc_boil_time(t_target:int=80,t_fridge:int=5,alt:int=175,weight=64)->float:
-  """
-  t_target: #egg temp
-  t_fridge: temp in fridge
-  alt: altitude above sea level
-  weight: #weight of one egg
-  """
-  pw = math.pow(weight,2.0/3.0)
-  af = (100- alt*0.003354-tFridge)
-  at = (100-alt*0.003354-tTarget)
-  _time = (0.451*pw*math.log(0.76*af/at)); #время варки вкрутую
-  return round(_time,2) #in minutes
+def calc_boil_time(t_target: int = 80, t_fridge: int = 5, alt: int = 175, weight=64) -> float:
+    """
+    t_target: int egg temp
+    t_fridge: int temp in fridge
+    alt: int altitude above sea level
+    weight: int weight of one egg
+    """
+    pw = math.pow(weight, 2.0 / 3.0)
+    af = (100 - alt * 0.003354 - t_fridge)
+    at = (100 - alt * 0.003354 - t_target)
+    _time = (0.451 * pw * math.log(0.76 * af / at))  # boil time for core temp t_target degree
+    return round(_time, 2)  # in minutes
 
 
 calc_boil_time()
